05eddd8f4acaf4d3d2ad35e4cf0d22bbcf0314ee
OguzhanAkcay <42253683+OguzhanAkcay@users.noreply.github.com>
2018-08-09 21:33:54 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -10,6 +10,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
 client.user.setGame("h!yardım | " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", "https://twitch.tv/scarew0");
-}
 client.user.setGame("7/24 | " + client.guilds.size + " Aktif " + client.users.size + " Bot ", "https://twitch.tv/scarew0");
 }
