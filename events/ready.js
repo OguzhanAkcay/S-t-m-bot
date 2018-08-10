1fc7ca9589304c76aed7504b2a2d92f58bdfc2c8
OguzhanAkcay <42253683+OguzhanAkcay@users.noreply.github.com>
2018-08-09 21:41:28 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,6 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
- client.user.setGame(Games[random], "https://www.twitch.tv/cansuoztrk");
-
+ client.user.setGame("h!yardım | Bot  7/24 aktif ", "https://www.twitch.tv/oguzhanbey123");
 }
